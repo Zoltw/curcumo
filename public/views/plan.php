d2621fd4954728ead082e1f2f18fb21bd2653dce
Filip Zolyniak <72789688+Zoltw@users.noreply.github.com>
2022-12-27 21:41:05 +0100

    added plan page grid and some photos

--- public/views/plan.php
+++ public/views/plan.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <head>
     <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <link rel="stylesheet" type="text/css" href="public/css/meal.css">
     <title>plan your meals</title>
     <link rel="icon" href="public/img/curcumo-logo.png" type="image/icon type">
 </head>
@@ -17,15 +18,86 @@
         </div>
     </div>
     <div class="plan-page-container">
-        <div class="pref-description">
+        <div class="draw-description">
             <h1>Plan your meals</h1>
         </div>
-        <div class="draw-meal">
-            <a class="contact-button">Draw your meal</a>
+        <div class="plate-container">
+            <div class="plate-icons">
+                <img class="icon" src="public/img/plate-icon.svg" alt="">
+                <img class="icon2" src="public/img/plate-icon.svg" alt="">
+                <img class="icon3" src="public/img/plate-icon.svg" alt="">
+            </div>
+            <div class="draw-button-container">
+                <a class="draw-button">draw your meal</a>
+            </div>
         </div>
-        
-
     </div>
+    <section class="meals-container">
+        <div id="meal-1">
+            <img src="public/img/meal-images/pesto-tagiatelle.jpg" alt="">
+            <div>
+                <h1>Pesto tagiatelle</h1>
+
+            </div>
+
+        </div>
+        <div id="meal-2">
+            <img src="public/img/meal-images/pesto-tagiatelle.jpg" alt="">
+            <div>
+                <h1>Pesto tagiatelle</h1>
+                
+            </div>
+            
+        </div>
+        <div id="meal-3">
+            <img src="public/img/meal-images/pesto-tagiatelle.jpg" alt="">
+            <div>
+                <h1>Pesto tagiatelle</h1>
+                
+            </div>
+            
+        </div>
+        <div id="meal-4">
+            <img src="public/img/meal-images/pesto-tagiatelle.jpg" alt="">
+            <div>
+                <h1>Pesto tagiatelle</h1>
+                
+            </div>
+            
+        </div>
+        <div id="meal-5">
+            <img src="public/img/meal-images/pesto-tagiatelle.jpg" alt="">
+            <div>
+                <h1>Pesto tagiatelle</h1>
+                
+            </div>
+        
+        </div>
+        <div id="meal-6">
+            <img src="public/img/meal-images/pesto-tagiatelle.jpg" alt="">
+            <div>
+                <h1>Pesto tagiatelle</h1>
+                
+            </div>
+            
+        </div>
+        <div id="meal-7">
+            <img src="public/img/meal-images/pesto-tagiatelle.jpg" alt="">
+            <div>
+                <h1>Pesto tagiatelle</h1>
+                
+            </div>
+        
+        </div>
+        <div id="meal-8">
+            <img src="public/img/meal-images/pesto-tagiatelle.jpg" alt="">
+            <div>
+                <h1>Pesto tagiatelle</h1>
+                
+            </div>
+        
+        </div>
+    </section>
     <div class="contact-button-container-pref">
         <a class="contact-button">Contact us</a>
         <a class="contact-button">Report a bug</a>
