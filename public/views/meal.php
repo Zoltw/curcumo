40523867ad0b24d0780f9ceaeff08820afb64e78
Filip Zolyniak <72789688+Zoltw@users.noreply.github.com>
2022-12-28 23:39:31 +0100

    added template to rest of views

---
+++ public/views/meal.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <link rel="stylesheet" type="text/css" href="public/css/meal.css">
+    <title>plan your meals</title>
+    <link rel="icon" href="public/img/curcumo-logo.png" type="image/icon type">
+</head>
+<body>
+<div class="container">
+    <div class="psc-user-container">
+        <div class="psc-icons">
+            <img class="plan-icon" src="public/img/plan-list.svg" alt="">
+            <img class="shop-icon" src="public/img/shop-icon.svg" alt="">
+            <img class="cook-icon" src="public/img/cook-icon.svg" alt="">
+        </div>
+        <div class="user-icon">
+            <img src="public/img/user-icon.svg" alt="">
+        </div>
+    </div>
+</div>
+</body> 
