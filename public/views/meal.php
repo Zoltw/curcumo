74b6e4b5a342a16077288ed1a46433c45e470683
Filip Zolyniak <72789688+Zoltw@users.noreply.github.com>
2022-12-29 23:36:16 +0100

    added some new content on meal page

--- public/views/meal.php
+++ public/views/meal.php
@@ -17,20 +17,38 @@
             <img src="public/img/user-icon.svg" alt="">
         </div>
     </div>
-    <section class="meals-container">
+    <div class="back-button">
+        <a class="backButton">back</a>
+    </div>
+    <section class="meal-section">
         <div id="meal-1">
             <img class="meal-img" src="public/img/meal-images/pesto-tagiatelle.jpg" alt="">
-            <div class="meal-description">
-                <h2>Pesto tagiatelle</h2>
-            </div>
-            <div class="time-prepare">
-                <div class="property-meal">
-                    <img class="clock-icon" src="public/img/clock-icon.svg" alt="">
-                    <h3>40 min</h3>
+            <div>
+                <div class="meal-description">
+                    <div class="meal-descript">
+                        <h2>Pesto tagiatelle</h2>
+                        <img class="mini-icon" src="public/img/vegetarian-icon.svg" alt="">
+                    </div>
+                    <div class="property-meal">
+                        <img class="clock-icon" src="public/img/clock-icon.svg" alt="">
+                        <h3>40 min</h3>
+                        <img class="clock-icon" src="public/img/hatLogo-mini.svg" alt="">
+                        <h3>easy</h3>
+                    </div>
+                    <p>1. Fry the nuts and seeds in a dry frying pan for a couple of minutes until dry.</p>
+                    <p>2. Make the sauce. Grind nuts, basil, garlic, seeds, cheese, butter, salt and pepper in a blender.</p>
+                    <p>3. At the same time, boil the pasta in boiling water according to the instructions on the package.</p>
+                    <p>4. Serve the pasta with the sauce. Sprinkle with cheese on top.</p>
+                    <p>5. Enjoy your meal!</p>
                 </div>
-                <div class="add-cont">
-                    <img class="mini-icon" src="public/img/vegetarian-icon.svg" alt="">
-                    <a class="addButton">add to list</a>
+                <div class="meal-products">
+                    <div class="border-top">
+                        <!-- <img class="mini-icon" src="public/img/vegetarian-icon.svg" alt="">
+                        <a class="addButton">add to list</a> -->
+                        <!-- <div class="border-top"></div>
+                        <div class="border-bottom"></div> -->
+                    </div>
+                    <div class="border-bottom"></div>
                 </div>
             </div>
         </div>
